8d9b78facc27e152a1cc701680e7ebbb9a2db35d
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-06-08 22:08:54 +0200

    mockdata

---
+++ server/database/User.js
@@ -0,0 +1,9 @@
+const userList = {
+    a: {
+        id: 'a',
+        username: 'irhamputra',
+        avatar: 'https://scontent-frt3-1.cdninstagram.com/vp/627949e7665e70c73a9c51c47fdd6bf7/5BB9418D/t51.2885-19/s150x150/34124457_220895758506864_6725624653290668032_n.jpg'
+    }
+};
+
+module.exports = userList;
