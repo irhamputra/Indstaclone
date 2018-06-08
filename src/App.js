107e35203d859dcaba6e43fc5b1a7d3f5a0503b5
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-06-09 01:22:14 +0200

    remove css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Posts from './components/Posts/Posts';
-import './App.css';
 
 class App extends Component {
   render() {
