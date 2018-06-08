2c339f98f374cb62bd192258f3fc0a3b788269bf
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-06-09 01:26:01 +0200

    single post component

---
+++ src/components/Post/Post.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        const { username, avatar, caption, image } = this.props;
+        return(
+            <article className="Post" ref="Post">
+                <header>
+                    <div className="Post-user">
+                        <div className="Post-user-avatar">
+                            <img src={avatar} alt={username} />
+                        </div>
+                        <div className="Post-user-nickname">
+                            <span>{username}</span>
+                        </div>
+                    </div>
+                </header>
+                <div className="Post-image">
+                    <div className="Post-image-bg">
+                        <img alt={caption} src={image} />
+                    </div>
+                </div>
+                <div className="Post-caption">
+                    <strong>{username}</strong> {caption}
+                </div>
+            </article>
+        )
+    }
+}
+
+export default Post
