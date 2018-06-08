926cc4107eb81abe2aac0041f93b1221a79d0d04
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-06-09 01:53:34 +0200

    reformat code

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Post extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
-        const { username, avatar, caption, image } = this.props;
-        return(
+    render() {
+        const {username, avatar, caption, image} = this.props;
+        return (
             <article className="Post" ref="Post">
                 <header>
                     <div className="Post-user">
                         <div className="Post-user-avatar">
-                            <img src={avatar} alt={username} />
+                            <img src={avatar} alt={username}/>
                         </div>
                         <div className="Post-user-nickname">
                             <span>{username}</span>
@@ -21,7 +21,7 @@ class Post extends Component {
                 </header>
                 <div className="Post-image">
                     <div className="Post-image-bg">
-                        <img alt={caption} src={image} />
+                        <img alt={caption} src={image}/>
                     </div>
                 </div>
                 <div className="Post-caption">
