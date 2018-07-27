8032e820b8d86b77c3ba3d3cdb88105e6b7f23c8
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-07-27 13:19:11 +0200

    refactoring

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 
 class Post extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         const {username, avatar, caption, image} = this.props;
         return (
